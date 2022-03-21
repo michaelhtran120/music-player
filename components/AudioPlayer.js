fcff487b07e5a8587a48c4c770db9e5dbed27233
Michael Tran <michaelhtran120@gmail.com>
2022-03-21 12:53:00 -0700

    reset song to beginning after it ends

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import song1 from '../assets/ArsGratiaArtis.mp3';
 import styles from '../styles/AudioPlayer.module.scss';
 
@@ -17,6 +17,25 @@ function AudioPlayer() {
     setIsPlaying((prevValue) => !prevValue);
   };
 
+  const handleRangeChange = () => {
+    audioPlayerRef.current.currentTime = progressBarRef.current.value;
+    changePlayerCurrentTime();
+  };
+
+  const changePlayerCurrentTime = useCallback(() => {
+    progressBarRef.current.style.setProperty(
+      '--progressWidth',
+      `${(progressBarRef.current.value / totalTime) * 100}%`,
+    );
+    setCurrentTime(progressBarRef.current.value);
+  }, [totalTime]);
+
+  const whilePlaying = useCallback(() => {
+    progressBarRef.current.value = audioPlayerRef.current.currentTime;
+    changePlayerCurrentTime();
+    animationRef.current = requestAnimationFrame(whilePlaying);
+  }, [changePlayerCurrentTime]);
+
   useEffect(() => {
     if (isPlaying) {
       audioPlayerRef.current.play();
@@ -25,7 +44,7 @@ function AudioPlayer() {
       audioPlayerRef.current.pause();
       cancelAnimationFrame(animationRef.current);
     }
-  }, [isPlaying]);
+  }, [isPlaying, whilePlaying]);
 
   useEffect(() => {
     const seconds = Math.floor(audioPlayerRef.current.duration);
@@ -33,8 +52,14 @@ function AudioPlayer() {
 
     // setting max of the input range.
     progressBarRef.current.max = seconds;
-    console.log(progressBarRef.current.max);
   }, [audioPlayerRef?.current?.loadedmetadata, audioPlayerRef?.current?.readyState]);
+
+  useEffect(() => {
+    if (currentTime == totalTime) {
+      handlePlayPause();
+      audioPlayerRef.current.currentTime = 0;
+    }
+  }, [currentTime, totalTime]);
 
   const calculateTime = (sec) => {
     const minutes = Math.floor(sec / 60);
@@ -42,25 +67,6 @@ function AudioPlayer() {
     const seconds = sec % 60;
     const returnedSeconds = (seconds) => (seconds < 10 ? `0${seconds}` : `${seconds}`);
     return `${returnedMinutes(minutes)}:${returnedSeconds(seconds)}`;
-  };
-
-  const changePlayerCurrentTime = () => {
-    progressBarRef.current.style.setProperty(
-      '--progressWidth',
-      `${(progressBarRef.current.value / totalTime) * 100}%`,
-    );
-    setCurrentTime(progressBarRef.current.value);
-  };
-
-  const handleRangeChange = () => {
-    audioPlayerRef.current.currentTime = progressBarRef.current.value;
-    changePlayerCurrentTime();
-  };
-
-  const whilePlaying = () => {
-    progressBarRef.current.value = audioPlayerRef.current.currentTime;
-    changePlayerCurrentTime();
-    animationRef.current = requestAnimationFrame(whilePlaying);
   };
 
   return (
