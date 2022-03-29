4b6ce755518a1e2ee416dc7727f4c40e8ac17253
Michael Tran <michaelhtran120@gmail.com>
2022-03-29 14:14:53 -0700

    stylize nav links

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from '../../styles/Nav/Nav.module.scss';
 
 const Nav = () => {
+  const router = useRouter();
+  console.log(router.pathname);
   return (
     <nav className={styles.nav}>
       <ul>
-        <li>
+        <li className={router.pathname == '/' ? styles.activeLink : styles.inactiveLink}>
           <Link href='/'>Home</Link>
         </li>
         <li>
           <h1>M's Sounds</h1>
         </li>
-        <li>
+        <li className={router.pathname == '/about' ? styles.activeLink : styles.inactiveLink}>
           <Link href='/about'>About</Link>
         </li>
       </ul>
